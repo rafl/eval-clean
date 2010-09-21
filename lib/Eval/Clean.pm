27aee22e96fd8e2c90338a6430c323c1b7b7e0f4
Florian Ragwitz <rafl@debian.org>
2010-09-21 05:38:49 +0200

    Allow bootstrapping xs code with and without dzil

--- lib/Eval/Clean.pm
+++ lib/Eval/Clean.pm
@@ -4,8 +4,13 @@ use strict;
 use warnings;
 use XSLoader;
 
-our $VERSION;
-
-XSLoader::load 'Eval::Clean', $Eval::Clean::VERSION;
+XSLoader::load(
+    __PACKAGE__,
+    # we need to be careful not to touch $VERSION at compile time, otherwise
+    # DynaLoader will assume it's set and check against it, which will cause
+    # fail when being run in the checkout without dzil having set the actual
+    # $VERSION
+    exists $Eval::clean::{VERSION} ? ${ $Eval::clean::{VERSION} } : (),
+);
 
 1;
